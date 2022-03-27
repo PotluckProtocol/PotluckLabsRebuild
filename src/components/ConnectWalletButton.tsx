e53792bea22c7d1f72e675b3df069e3d88bc1673
Quu <cryptoquu@gmail.com>
2022-03-27 12:54:21 +0300

    Add current network icon into connect button when connected

--- src/components/ConnectWalletButton.tsx
+++ src/components/ConnectWalletButton.tsx
@@ -1,13 +1,42 @@
 import { useContext } from "react";
+import styled from "styled-components";
 import { AccountContext } from "../api/account/AccountContext";
 import Button from "./Button";
+import { NetworkIcon } from "./NetworkIcon";
 
 const toShortWallet = (walletAddr: string): string => {
     return [
         walletAddr.substring(0, 4),
-        walletAddr.substring(walletAddr.length - 6)
+        walletAddr.substring(walletAddr.length - 4)
     ].join('...');
 }
+
+type ButtonGroupProps = {
+    singleMode: boolean;
+}
+
+const ButtonGroup = styled.div<ButtonGroupProps>`
+    ${props => (!props.singleMode) && `
+        button:not(:last-child) {
+            border-right: 0;
+        }
+
+        button:first-child {
+            border-top-right-radius: 0;
+            border-bottom-right-radius: 0; 
+        }
+
+        button:last-child {
+            border-right: inherit;
+            border-top-left-radius: 0;
+            border-bottom-left-radius: 0; 
+        }
+    `}
+`;
+
+const NetworkButton = styled(Button)`
+    padding: 0 6px;
+`;
 
 export const ConnectWalletButton: React.FC = () => {
     const accountContext = useContext(AccountContext);
@@ -19,13 +48,23 @@ export const ConnectWalletButton: React.FC = () => {
     let connectButtonText = 'Connect Wallet';
     if (accountContext.isConnecting) {
         connectButtonText = 'Connecting...';
-    } else if (accountContext.account) {
+    } else if (!!accountContext.account) {
         connectButtonText = toShortWallet(accountContext.account.walletAddress);
     }
 
+    // Single button mode if there is no account connected
+    const useSingleButtonMode = !accountContext.account;
+
     return (
-        <Button variant="outlined" onClick={handleConnectClick}>
-            {connectButtonText}
-        </Button>
+        <ButtonGroup singleMode={useSingleButtonMode} className="flex justify-center items-center">
+            <Button variant="outlined" onClick={handleConnectClick}>
+                {connectButtonText}
+            </Button>
+            {(!!accountContext.account) && (
+                <NetworkButton>
+                    <NetworkIcon networkId={accountContext.account.network.id} size={30} />
+                </NetworkButton>
+            )}
+        </ButtonGroup>
     )
 }
---
+++ src/components/NetworkIcon.tsx
@@ -0,0 +1,35 @@
+
+
+const ICON_MAP: { [networkId: number]: string } = {
+    // Matic
+    137: '/images/Networks/matic.svg',
+
+    // Fantom
+    250: '/images/Networks/fantom.svg',
+
+    // Avalanche
+    43114: '/images/Networks/avalanche.svg'
+}
+
+export type NetworkIconProps = {
+    className?: string;
+    size?: number,
+    networkId: number;
+}
+
+export const NetworkIcon: React.FC<NetworkIconProps> = ({
+    className,
+    networkId,
+    size
+}) => {
+    size = size || 40;
+
+    const imageUrl = ICON_MAP[networkId];
+    if (!imageUrl) {
+        throw new Error(`Could not resolve icon for network ${networkId}`);
+    }
+
+    return (
+        <img width={size} className={className} src={imageUrl} />
+    );
+}
