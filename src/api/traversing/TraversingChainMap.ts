dbdeab6871e4db09d27db6e185d91aaca07750cf
Quu <cryptoquu@gmail.com>
2022-09-15 20:28:38 +0300

    Patch layer0 chain ids

--- src/api/traversing/TraversingChainMap.ts
+++ src/api/traversing/TraversingChainMap.ts
@@ -8,15 +8,15 @@ export type TraversingChainInfo = {
 
 export const TRAVERSING_CHAIN_MAP: TraversingChainInfo[] = [{
     network: 'FTM',
-    traverseChainId: 12,
+    traverseChainId: 112,
     endpoint: '0xb6319cC6c8c27A8F5dAF0dD3DF91EA35C4720dd7'
 }, {
     network: 'AVAX',
-    traverseChainId: 6,
+    traverseChainId: 106,
     endpoint: '0x3c2269811836af69497E5F486A85D7316753cf62'
 }, {
     network: 'MATIC',
-    traverseChainId: 9,
+    traverseChainId: 109,
     endpoint: '0x3c2269811836af69497E5F486A85D7316753cf62'
 }]
 
